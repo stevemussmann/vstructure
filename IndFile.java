d3d0a01649c859ada3b181d30654af67e7a642a4
Steve Mussmann <smussmann@gmail.com>
2019-01-14 10:15:36 -0700

    Indfile data now parsed and stored in data structure

--- IndFile.java
+++ IndFile.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class IndFile {
 	String fn;
+	HashMap<String, HashMap<String, double[]>> data;
 
 	IndFile(String f){
 		this.fn = f;
@@ -13,14 +16,50 @@ public class IndFile {
 		Scanner s;
 		try{
 			s = new Scanner(new File(this.fn));
+			data = new HashMap<String, HashMap<String, double[]>>();
 			while(s.hasNextLine()) {
 				String line = s.nextLine();
-				System.out.println(line);
+				this.parseLine(line);
 			}
 		}catch(FileNotFoundException e){
 			e.printStackTrace(System.err);
 			System.exit(1);
 		}
+	}
+
+	void parseLine(String l) {
+		//System.out.println(l);
+		String[] sl = l.split("\\s+"); //split line
+		String[] subset = Arrays.copyOfRange(sl, 5, sl.length); //subset columns w/ancestry
+
+		double[] ancestry = this.DoubleArray(subset); //convert String array to double array
+		System.out.println(sl[1].toString() + "," + Arrays.toString(ancestry)); //test print
+
+		//put data into HashMap of HashMaps of arrays
+		//key 1 = sample pop
+		//key 2 = individual ID
+		//value = ancestry
+		if(!(data.containsKey(sl[3]))){
+			//System.out.println("Line reached (empty)");
+			data.put(sl[3], new HashMap<String, double[]>());
+			data.get(sl[3]).put(sl[1],ancestry);
+		}else{
+			//System.out.println(sl[3] + " is not emtpy");
+			data.get(sl[3]).put(sl[1],ancestry);
+			
+		}
+
+	}
+
+
+	double[] DoubleArray(String[] array) {
+		double[] converted = new double[array.length];
+
+		for(int i=0; i < array.length; i++) {
+			converted[i] = Double.parseDouble(array[i]);
+		}
+
+		return converted;
 	}
 	
 }
