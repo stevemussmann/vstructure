04bf01d56d3b0807b780e803cdea08a7a611dd81
Steve Mussmann <smussmann@gmail.com>
2019-01-14 15:15:56 -0700

    can move populations between the two JLists

--- View.java
+++ View.java
@@ -1,11 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-class View extends JPanel{
+class View extends JPanel implements ActionListener{
+	PopList pl, rm;
+	JButton buttonIn, buttonOut;
 	
 	View(){
 		
-		PopList pl = new PopList();
+		this.pl = new PopList();
+		this.rm = new PopList();
+		this.rm.setModel(new DefaultListModel());
 
 		TextField tf = new TextField(20);
 		this.add(new StaticTextField("Path to Clumpp Ind File:")); //add static text field
@@ -15,7 +23,56 @@ class View extends JPanel{
 		//this.add(new ParseFileButton("Read File", tf));
 
 		JScrollPane plScroll = new JScrollPane(pl);
+		JScrollPane rmScroll = new JScrollPane(rm);
+
+		this.buttonIn = new JButton(">>");
+		this.buttonIn.addActionListener(this);
+		
+		this.buttonOut = new JButton("<<");
+		this.buttonOut.addActionListener(this);
+
 		this.add(plScroll);
+		this.add(buttonIn);
+		this.add(buttonOut);
+		this.add(rmScroll);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+
+		int i=0;
+
+		DefaultListModel plModel = (DefaultListModel)this.pl.getModel();
+		DefaultListModel rmModel = (DefaultListModel)this.rm.getModel();
+
+		if(e.getSource() == this.buttonIn){
+			int[] fromindex = this.pl.getSelectedIndices();
+			Object[] from = this.pl.getSelectedValues();
+
+			for(i=0; i<from.length; i++){
+				rmModel.addElement(from[i]);
+			}
+
+			for(i= (fromindex.length-1); i>=0; i--){
+				plModel.remove(fromindex[i]);
+			}
+
+		}
+
+		else if(e.getSource() == buttonOut){
+			Object[] to = this.rm.getSelectedValues();
+			int[] toindex = this.rm.getSelectedIndices();
+
+			for(i=0; i<to.length; i++){
+				plModel.addElement(to[i]);
+			}
+
+			for(i= (toindex.length-1); i>=0; i--){
+				rmModel.remove(toindex[i]);
+			}
+
+		}
 
 	}
 	
